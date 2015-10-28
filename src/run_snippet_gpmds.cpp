8ff562800af23a654c9d3c4a4052f593161b5ec5
Felix Duvallet <felix.duvallet@epfl.ch>
2015-10-28 16:23:00 +0100

    Run code.

--- src/run_snippet_gpmds.cpp
+++ src/run_snippet_gpmds.cpp
@@ -4,31 +4,22 @@
 
 
 #include <iostream>
-#include "snippet_gpmds.h"
 #include <locally_modulated_ds/gp_modulated_ds.h>
+#include "snippet_gpmds/snippet_gpmds.h"
 
 using GPMDS = GaussianProcessModulatedDS<double>;
 using Vec3 = GPMDS::Vec;
 
-/*
- * Vec3 GPD
-        GaussianProcessModulatedDS::DynamicalSystem original_dynamics(
-        GaussianProcessModulatedDS::Vec) {
-  GaussianProcessModulatedDS::Vec ret;
-  return ret;
+Vec3 original_dynamics(Vec3 pose) {
+  return -1.0 * pose;
 }
- */
-
-//Vec3
-
-
 
 int main() {
-  printf("hello world");
+  printf("Snippet GPMDS");
 
-  //std::function<Vec3(Vec3)> dynamics_fun = original_dynamics;
+  std::function<Vec3(Vec3)> dynamics_fun = original_dynamics;
 
-  //SnippetGPMDS snippet_gpmds(dynamics_fun);
+  SnippetGPMDS snippet_gpmds(dynamics_fun);
 
   return 0;
 }
