cfe8c700600fda553e7065a8cc4768d08f891b09
Felix Duvallet <felix.duvallet@epfl.ch>
2015-10-28 15:59:31 +0100

    Code reformatting, renamed typedef.

--- include/locally_modulated_ds/gp_modulated_ds.h
+++ include/locally_modulated_ds/gp_modulated_ds.h
@@ -6,28 +6,35 @@
 #include "gaussian_process_regression/gaussian_process_regression.h"
 #include <memory>
 
-template <typename realtype>
-class GaussianProcessModulatedDS : public LocallyModulatedDS<Eigen::Matrix<realtype,3,1>,Eigen::Matrix<realtype,3,3>>
-{
+template<typename realtype>
+class GaussianProcessModulatedDS
+        : public LocallyModulatedDS<Eigen::Matrix<realtype, 3, 1>,
+                Eigen::Matrix<realtype, 3, 3>> {
 
  private:
-  static const realtype MIN_ANGLE;
-  std::unique_ptr<GaussianProcessRegression<realtype> > gpr_;
-  
+    static const realtype MIN_ANGLE;
+    std::unique_ptr<GaussianProcessRegression<realtype> > gpr_;
+
  public:
 
-    typedef Eigen::Matrix<realtype,3,1> Vec;
-    typedef Eigen::Matrix<realtype,3,3> Mat;
+    // Use Eigen for the locally-modulated DS types.
+    typedef Eigen::Matrix<realtype, 3, 1> Vec;
+    typedef Eigen::Matrix<realtype, 3, 3> Mat;
+
+    // A Dynamical System is a function that maps position to velocity.
     typedef std::function<Vec(Vec)> DynamicalSystem;
 
-    GaussianProcessModulatedDS(DynamicalSystem original_dynamics) : LocallyModulatedDS<Vec,Mat>(original_dynamics), gpr_(new GaussianProcessRegression<realtype>(3,3)){};
-  
+    GaussianProcessModulatedDS(DynamicalSystem original_dynamics)
+            : LocallyModulatedDS<Vec, Mat>(original_dynamics),
+              gpr_(new GaussianProcessRegression<realtype>(3, 3)) { };
 
-  virtual ~GaussianProcessModulatedDS(){};
-  virtual Mat ModulationFunction(const Vec& in); 
-  virtual void AddData(const Vec& new_pos, const Vec& new_vel);
+    virtual ~GaussianProcessModulatedDS() { };
+
+    virtual Mat ModulationFunction(const Vec &in);
+
+    virtual void AddData(const Vec &new_pos, const Vec &new_vel);
 };
 
 #include "locally_modulated_ds/gp_modulated_ds.hxx"
-#endif /* _GP_MODULATED_DS_H_ */
 
+#endif // _GP_MODULATED_DS_H_
