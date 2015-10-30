bd8a254280da859cdd269015a25551affd387313
Felix Duvallet <felix.duvallet@epfl.ch>
2015-10-30 10:31:02 +0100

    Turned GPMDS unit tests into a fixtured class.

--- test/test_gp_modulated_ds.cpp
+++ test/test_gp_modulated_ds.cpp
@@ -31,6 +31,121 @@ Mat3 load_matrix_3x3(const char *fname) {
   return mat;
 }
 
+Vec3 basic_dynamics(Vec3 pos) {
+  return -1.0 * pos;
+}
+
+template<typename M>
+void compare_matrices(M m1, M m2) {
+  ASSERT_EQ(m1.rows(), m2.rows());
+  ASSERT_EQ(m1.cols(), m2.cols());
+  for (size_t row = 0; row < m1.rows(); row++) {
+    for (size_t col = 0; col < m1.rows(); col++) {
+      ASSERT_NEAR(m1(row, col), m2(row, col), FLOAT_COMPARISON_THRESHOLD);
+    }
+  }
+}
+
+class GPMDSTest : public ::testing::Test {
+
+    void SetUp() {
+      gp_mds_.reset(new GaussianProcessModulatedDS<float>(basic_dynamics));
+    }
+
+ public:
+    std::unique_ptr<GaussianProcessModulatedDS<float>> gp_mds_;
+    
+};
+
+}  // namespace
+
+
+TEST_F(GPMDSTest, DynamicsTestZero) {
+  Vec3 vector = Vec3::Zero();
+  EXPECT_NEAR(0, vector[0], FLOAT_COMPARISON_THRESHOLD);
+  EXPECT_NEAR(0, vector[1], FLOAT_COMPARISON_THRESHOLD);
+  EXPECT_NEAR(0, vector[2], FLOAT_COMPARISON_THRESHOLD);
+
+  Vec3 output = basic_dynamics(vector);
+  EXPECT_NEAR(0, output[0], FLOAT_COMPARISON_THRESHOLD);
+  EXPECT_NEAR(0, output[1], FLOAT_COMPARISON_THRESHOLD);
+  EXPECT_NEAR(0, output[2], FLOAT_COMPARISON_THRESHOLD);
+}
+
+TEST_F(GPMDSTest, DynamicsTestStuff) {
+  Vec3 vector;
+  vector << 1, 5, 10;
+  EXPECT_NEAR(1, vector[0], FLOAT_COMPARISON_THRESHOLD);
+  EXPECT_NEAR(5, vector[1], FLOAT_COMPARISON_THRESHOLD);
+  EXPECT_NEAR(10, vector[2], FLOAT_COMPARISON_THRESHOLD);
+
+  Vec3 output = basic_dynamics(vector);
+  EXPECT_NEAR(-1, output[0], FLOAT_COMPARISON_THRESHOLD);
+  EXPECT_NEAR(-5, output[1], FLOAT_COMPARISON_THRESHOLD);
+  EXPECT_NEAR(-10, output[2], FLOAT_COMPARISON_THRESHOLD);
+}
+
+TEST_F(GPMDSTest, CreateObject) {
+  GaussianProcessModulatedDS<float> gpmds(basic_dynamics);
+  ASSERT_NE(nullptr, gpmds.getOriginalDynamics());
+  ASSERT_TRUE((bool) gpmds.getOriginalDynamics());
+}
+
+TEST_F(GPMDSTest, GetOutputZeroTest) {
+  // Call getOutput with zero input.
+  Vec3 x = gp_mds_->GetOutput(Vec3::Zero());
+  ASSERT_NEAR(0, x[0], FLOAT_COMPARISON_THRESHOLD);
+  ASSERT_NEAR(0, x[1], FLOAT_COMPARISON_THRESHOLD);
+  ASSERT_NEAR(0, x[2], FLOAT_COMPARISON_THRESHOLD);
+}
+
+TEST_F(GPMDSTest, GetOutputStuffTest) {
+  // Call getOutput with some input stuff.
+  GaussianProcessModulatedDS<float> gpmds(basic_dynamics);
+  Vec3 vector;
+  vector << 1, 2, 3;
+  auto x =gp_mds_->GetOutput(vector);
+  EXPECT_NEAR(-1, x[0], FLOAT_COMPARISON_THRESHOLD);
+  EXPECT_NEAR(-2, x[1], FLOAT_COMPARISON_THRESHOLD);
+  EXPECT_NEAR(-3, x[2], FLOAT_COMPARISON_THRESHOLD);
+}
+
+TEST_F(GPMDSTest, FindFiles) {
+  std::vector<string> file_names;
+  file_names.push_back("R45z.txt");
+  file_names.push_back("R162z.txt");
+  for (string f:file_names) {
+    ifstream ff(f);
+    ASSERT_TRUE(ff);
+  }
+}
+
+TEST_F(GPMDSTest, PureRotation45) {
+  GaussianProcessModulatedDS<float> gpmds(basic_dynamics);
+  auto correct_result = load_matrix_3x3("R45z.txt");
+  Vec3 z_axis_45;
+  z_axis_45.setZero();
+  z_axis_45(2) = deg_to_rad(45.0);
+  auto gpmds_result = gpmds.ModulationFunction(z_axis_45, 0.0);
+  compare_matrices(gpmds_result, correct_result);
+}
+
+TEST_F(GPMDSTest, PureRotation162) {
+  GaussianProcessModulatedDS<float> gpmds(basic_dynamics);
+  auto correct_result = load_matrix_3x3("R162z.txt");
+  Vec3 z_axis_162;
+  z_axis_162.setZero();
+  z_axis_162(2) = deg_to_rad(162.0);
+  auto gpmds_result = gpmds.ModulationFunction(z_axis_162, 0.0);
+  compare_matrices(gpmds_result, correct_result);
+}
+
+
+int main(int argc, char *argv[]) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
+
 // void load_data(const char *fname, vector<input_type> &inputs, vector<output_type> &outputs, int input_dim, int output_dim) {
 //   input_type inp,tinp;
 //   output_type outp,toutp;
@@ -47,108 +162,3 @@ Mat3 load_matrix_3x3(const char *fname) {
 //     outputs.push_back(outp);
 //   }
 // }
-
-
-Vec3 basic_dynamics(Vec3 pos) {
-  return -1.0 * pos;
-}
-
-template<typename M>
-void compare_matrices(M m1, M m2) {
-  ASSERT_EQ(m1.rows(), m2.rows());
-  ASSERT_EQ(m1.cols(), m2.cols());
-  for (size_t row = 0; row < m1.rows(); row++) {
-    for (size_t col = 0; col < m1.rows(); col++) {
-      ASSERT_NEAR(m1(row, col), m2(row, col), FLOAT_COMPARISON_THRESHOLD);
-    }
-  }
-}
-
-TEST(Basic, DynamicsTestZero) {
-  Vec3 vector = Vec3::Zero();
-  EXPECT_NEAR(0, vector[0], FLOAT_COMPARISON_THRESHOLD);
-  EXPECT_NEAR(0, vector[1], FLOAT_COMPARISON_THRESHOLD);
-  EXPECT_NEAR(0, vector[2], FLOAT_COMPARISON_THRESHOLD);
-
-  Vec3 output = basic_dynamics(vector);
-  EXPECT_NEAR(0, output[0], FLOAT_COMPARISON_THRESHOLD);
-  EXPECT_NEAR(0, output[1], FLOAT_COMPARISON_THRESHOLD);
-  EXPECT_NEAR(0, output[2], FLOAT_COMPARISON_THRESHOLD);
-}
-
-TEST(Basic, DynamicsTestStuff) {
-  Vec3 vector;
-  vector << 1, 5, 10;
-  EXPECT_NEAR(1, vector[0], FLOAT_COMPARISON_THRESHOLD);
-  EXPECT_NEAR(5, vector[1], FLOAT_COMPARISON_THRESHOLD);
-  EXPECT_NEAR(10, vector[2], FLOAT_COMPARISON_THRESHOLD);
-
-  Vec3 output = basic_dynamics(vector);
-  EXPECT_NEAR(-1, output[0], FLOAT_COMPARISON_THRESHOLD);
-  EXPECT_NEAR(-5, output[1], FLOAT_COMPARISON_THRESHOLD);
-  EXPECT_NEAR(-10, output[2], FLOAT_COMPARISON_THRESHOLD);
-}
-
-TEST(Basic, CreateObject) {
-  GaussianProcessModulatedDS<float> gpmds(basic_dynamics);
-  ASSERT_NE(nullptr, gpmds.getOriginalDynamics());
-  ASSERT_TRUE((bool) gpmds.getOriginalDynamics());
-}
-
-TEST(Basic, GetOutputZeroTest) {
-  // Call getOutput with zero input.
-  GaussianProcessModulatedDS<float> gpmds(basic_dynamics);
-  Vec3 x = gpmds.GetOutput(Vec3::Zero());
-  ASSERT_NEAR(0, x[0], FLOAT_COMPARISON_THRESHOLD);
-  ASSERT_NEAR(0, x[1], FLOAT_COMPARISON_THRESHOLD);
-  ASSERT_NEAR(0, x[2], FLOAT_COMPARISON_THRESHOLD);
-}
-
-TEST(Basic, GetOutputStuffTest) {
-  // Call getOutput with some input stuff.
-  GaussianProcessModulatedDS<float> gpmds(basic_dynamics);
-  Vec3 vector;
-  vector << 1, 2, 3;
-  auto x = snippet_gpmds_->gp_mds_->GetOutput(vector);
-  EXPECT_NEAR(-1, x[0], EPS_THRESH);
-  EXPECT_NEAR(-2, x[1], EPS_THRESH);
-  EXPECT_NEAR(-3, x[2], EPS_THRESH);
-}
-
-TEST(Basic, FindFiles) {
-  std::vector<string> file_names;
-  file_names.push_back("R45z.txt");
-  file_names.push_back("R162z.txt");
-  for (string f:file_names) {
-    ifstream ff(f);
-    ASSERT_TRUE(ff);
-  }
-}
-
-TEST(ModulationFunction, PureRotation45) {
-  GaussianProcessModulatedDS<float> gpmds(basic_dynamics);
-  auto correct_result = load_matrix_3x3("R45z.txt");
-  Vec3 z_axis_45;
-  z_axis_45.setZero();
-  z_axis_45(2) = deg_to_rad(45.0);
-  auto gpmds_result = gpmds.ModulationFunction(z_axis_45, 0.0);
-  compare_matrices(gpmds_result, correct_result);
-}
-
-TEST(ModulationFunction, PureRotation162) {
-  GaussianProcessModulatedDS<float> gpmds(basic_dynamics);
-  auto correct_result = load_matrix_3x3("R162z.txt");
-  Vec3 z_axis_162;
-  z_axis_162.setZero();
-  z_axis_162(2) = deg_to_rad(162.0);
-  auto gpmds_result = gpmds.ModulationFunction(z_axis_162, 0.0);
-  compare_matrices(gpmds_result, correct_result);
-}
-
-
-};
-
-int main(int argc, char *argv[]) {
-  testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
-}
